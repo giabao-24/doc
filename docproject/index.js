f7011d1cfa1507d1057c4849b663ff2e67f70422
giabao-24 <baocaohuy123@gmail.com>
2025-01-23 11:39:01 +0700

    Add files via upload

---
+++ docproject/index.js
@@ -0,0 +1,24 @@
+function tradao() {
+  window.location.href = "payment.html";
+}
+function sinhtobo() {
+  window.location.href = "payment2.html";
+}
+function duahau() {
+  window.location.href = "payment3.html";
+}
+function trasua() {
+  window.location.href = "payment4.html";
+}
+function xucxich() {
+  window.location.href = "payforsausage";
+}
+function changa() {
+  window.location.href = "payforchickenleg";
+}
+function banhtrangtrung() {
+  window.location.href = "payforbanhtrangtrung";
+}
+function banhtrangtron() {
+  window.location.href = "payforbanhtrangtron";
+}
